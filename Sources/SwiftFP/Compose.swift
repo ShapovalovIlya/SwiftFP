3d09e090754d80a37e671ffcf27f66f83e9db7d7
Ilia Shapovalov <greateman@mail.ru>
2023-08-19 14:32:19 +0300

    Roll back tryCompose func

--- Sources/SwiftFP/Compose.swift
+++ Sources/SwiftFP/Compose.swift
@@ -6,16 +6,32 @@
 //
 
 public func compose<A,B,C>(
-    _ f: @escaping (A) throws -> B,
-    _ g: @escaping (B) throws -> C
-) rethrows -> (A) throws -> C {
-    { try g(f($0)) }
+    _ f: @escaping (A) -> B,
+    _ g: @escaping (B) -> C
+) -> (A) -> C {
+    { g(f($0)) }
 }
 
 public func compose<A,B,C,D>(
+    _ f: @escaping (A) -> B,
+    _ g: @escaping (B) -> C,
+    _ h: @escaping (C) -> D
+) -> (A) -> D {
+    { h(g(f($0))) }
+}
+
+public func tryCompose<A,B,C>(
+    _ f: @escaping (A) throws -> B,
+    _ g: @escaping (B) throws -> C
+) -> (A) throws -> C {
+    { try g(f($0)) }
+}
+
+public func tryCompose<A,B,C, D>(
     _ f: @escaping (A) throws -> B,
     _ g: @escaping (B) throws -> C,
     _ h: @escaping (C) throws -> D
 ) rethrows -> (A) throws -> D {
     { try h(g(f($0))) }
 }
+
