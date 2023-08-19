97e21088d0d07f05ac588dd1e5178557862e68a6
Ilia Shapovalov <greateman@mail.ru>
2023-08-19 14:25:15 +0300

    Update compose func.

--- Sources/SwiftFP/Compose.swift
+++ Sources/SwiftFP/Compose.swift
@@ -6,32 +6,16 @@
 //
 
 public func compose<A,B,C>(
-    _ f: @escaping (A) -> B,
-    _ g: @escaping (B) -> C
-) -> (A) -> C {
-    { g(f($0)) }
-}
-
-public func compose<A,B,C,D>(
-    _ f: @escaping (A) -> B,
-    _ g: @escaping (B) -> C,
-    _ h: @escaping (C) -> D
-) -> (A) -> D {
-    { h(g(f($0))) }
-}
-
-public func tryCompose<A,B,C>(
     _ f: @escaping (A) throws -> B,
     _ g: @escaping (B) throws -> C
-) -> (A) throws -> C {
+) rethrows -> (A) throws -> C {
     { try g(f($0)) }
 }
 
-public func tryCompose<A,B,C, D>(
+public func compose<A,B,C,D>(
     _ f: @escaping (A) throws -> B,
     _ g: @escaping (B) throws -> C,
     _ h: @escaping (C) throws -> D
-) -> (A) throws -> D {
+) rethrows -> (A) throws -> D {
     { try h(g(f($0))) }
 }
-
