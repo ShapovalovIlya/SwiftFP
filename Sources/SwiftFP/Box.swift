65c1943f8fa25023b34a620cf9c1bfdc17a1f2d6
Ilia Shapovalov <greateman@mail.ru>
2023-09-01 12:10:58 +0300

    Add box

--- Sources/SwiftFP/Box.swift
+++ Sources/SwiftFP/Box.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+public struct Box<T> {
+    public let value: T
+    
+    public init(_ value: T) {
+        self.value = value
+    }
+    
+    public func map<U>(_ transform: (T) throws -> U) rethrows -> Box<U> {
+        Box<U>(try transform(self.value))
+    }
+    
+    public func flatMap<U>(_ transform: (T) throws -> Box<U>) rethrows -> Box<U> {
+        try transform(self.value)
+    }
+    
+    public func apply<U>(_ transform: Box<(T) -> U>) -> Box<U> {
+        Box<U>(transform.value(self.value))
+    }
+}
