56e880ed75343f1df07d330de9179eb7666433c7
Ilia Shapovalov <greateman@mail.ru>
2024-05-08 21:02:19 +0300

    Add forEach() methods to AsyncSequence

--- Sources/SwiftFP/Tools/Monad/AsyncSequence.swift
+++ Sources/SwiftFP/Tools/Monad/AsyncSequence.swift
@@ -8,10 +8,24 @@
 import Foundation
 
 public extension AsyncSequence {
+    
+    /// Calls the given closure on each element in the sequence in the same order as a for-in loop.
+    /// - Parameter body: A asynchronous closure that takes an element of the sequence as a parameter.
     @inlinable
-    func forAwaitEach(_ body: (Element) throws -> Void) async rethrows {
+    func forEach(_ body: (Self.Element) async throws -> Void) async rethrows {
+        for try await element in self {
+            try await body(element)
+        }
+    }
+    
+    
+    /// Calls the given closure on each element in the sequence in the same order as a for-in loop.
+    /// - Parameter body: A closure that takes an element of the sequence as a parameter.
+    @inlinable
+    func forEach(_ body: (Self.Element) throws -> Void) async rethrows {
         for try await element in self {
             try body(element)
         }
     }
+    
 }
