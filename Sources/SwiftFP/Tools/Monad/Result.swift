a6f9a2283f5dcaf42785722acebb9ec8cb093f2a
Ilia Shapovalov <greateman@mail.ru>
2024-05-10 18:17:59 +0300

    Refactoring Result extensions: - rename asyncFlatMap to flatMap - rename asyncMap to map - add tests

--- Sources/SwiftFP/Tools/Monad/Result.swift
+++ Sources/SwiftFP/Tools/Monad/Result.swift
@@ -7,28 +7,53 @@
 
 import Foundation
  
-public extension Result {
+public extension Result where Failure == Error {
     
-    /// Returns a new result, asynchronous mapping any success value using the given `transformation` and unwrapping the produced result.
+    /// Asynchronously Creates a new result by evaluating a asynchronous throwing closure,
+    /// capturing the returned value as a success, or any thrown error as a failure.
+    /// - Parameter body: A asynchronous throwing closure to evaluate.
+    @inlinable
+    init(asyncCatch body: () async throws -> Success) async {
+        do {
+            let success = try await body()
+            self = .success(success)
+        } catch {
+            self = .failure(error)
+        }
+    }
+    
+    /// Returns a new result, asynchronous mapping any success value using the given `transformation` 
+    /// and unwrapping the produced result.
     /// - Parameter transform: A asynchronous closure that takes the success value of the instance.
     /// - Returns: A `Result` instance, either from the closure or the previous `.failure`.
     @inlinable
-    func asyncFlatMap<T>(
-        _ transform: (Success) async -> Result<T, Failure>
+    func flatMap<T>(
+        _ asyncTransform: (Success) async -> Result<T, Failure>
     ) async -> Result<T, Failure> {
         switch self {
         case .success(let success):
-            return await transform(success)
+            return await asyncTransform(success)
             
         case .failure(let failure):
             return .failure(failure)
         }
     }
     
+    /// Returns a new result, mapping any success value using the given asynchronous transformation.
+    /// - Parameter asyncTransform: A asynchronous closure that takes the success value of this instance.
+    /// - Returns: A Result instance with the result of evaluating `asyncTransform` as the new success value
+    /// if this instance represents a success.
     @inlinable
-    func asyncMap<T>(_ transform: (Success) async -> T) async -> Result<T, Failure> {
-        await asyncFlatMap { success in
-            await .success(transform(success))
+    func map<T>(_ asyncTransform: (Success) async -> T) async -> Result<T, Failure> {
+        await flatMap { success in
+            await .success(asyncTransform(success))
+        }
+    }
+    
+    @inlinable
+    func tryMap<T>(_ transform: (Success) async throws -> T) async -> Result<T, Failure> {
+        await flatMap { success in
+            await Result<T, Failure> { try await transform(success) }
         }
     }
     
@@ -47,28 +72,7 @@ public extension Result {
             other.map { (success, $0) }
         }
     }
-}
-
-public extension Result where Failure == Error {
     
-    /// Creates a new result by evaluating a asynchronous throwing closure, capturing the returned value as a success, or any thrown error as a failure.
-    /// - Parameter body: A asynchronous throwing closure to evaluate.
-    @inlinable
-    init(asyncCatch body: () async throws -> Success) async {
-        do {
-            let success = try await body()
-            self = .success(success)
-        } catch {
-            self = .failure(error)
-        }
-    }
-    
-    @inlinable
-    func asyncMap<T>(_ transform: (Success) async throws -> T) async -> Result<T, Failure> {
-        await asyncFlatMap { success in
-            await Result<T, Failure> { try await transform(success) }
-        }
-    }
 }
 
 public extension Result where Success == Data {
---
+++ Tests/SwiftFPTests/ResultTests.swift
@@ -0,0 +1,61 @@
+//
+//  ResultTests.swift
+//  SwiftFP
+//
+//  Created by Илья Шаповалов on 10.05.2024.
+//
+
+import XCTest
+import SwiftFP
+
+final class ResultTests: XCTestCase {
+    //MARK: - flatMap(_:)
+    func test_asyncFlatMap_Success() async {
+        let sut = await Result<Int, Error>
+            .success(1)
+            .flatMap(asyncAddOneSuccess(_:))
+        
+        switch sut {
+        case .success(let success):
+            XCTAssertEqual(success, 2)
+            
+        case .failure: XCTFail()
+        }
+    }
+    
+    func test_asyncFlatMap_Fail() async {
+        let sut = await Result<Int, Error>
+            .success(1)
+            .flatMap(asyncAddOneFailure(_:))
+        
+        switch sut {
+        case .success: XCTFail()
+            
+        case .failure(let fail):
+            XCTAssertTrue(fail is TestFail)
+        }
+    }
+    
+    //MARK: - map(_:)
+    func test_asyncMap() async {
+        let sut = await Result<Int, Error>
+            .success(1)
+            .map(asyncAddOne(_:))
+        
+        switch sut {
+        case .success(let success):
+            XCTAssertEqual(success, 2)
+            
+        case .failure: XCTFail()
+        }
+    }
+}
+
+//MARK: - Helpers
+private extension ResultTests {
+    struct TestFail: Error {}
+    
+    func asyncAddOne(_ v: Int) async -> Int { v + 1 }
+    func asyncAddOneSuccess(_ v: Int) async -> Result<Int, Error> { .success(v + 1) }
+    func asyncAddOneFailure(_ v: Int) async -> Result<Int, Error> { .failure(TestFail()) }
+}
