c23a5e6b5557307156ca4dff64be11f8b7ec3adb
Shapovalov.i <isdaiquiri@gmail.com>
2025-03-19 17:11:37 +0300

    Add Validated init with reader objects

---
+++ Sources/SwiftFP/Extensions/Validated.swift
@@ -0,0 +1,22 @@
+//
+//  Validated.swift
+//  SwiftFP
+//
+//  Created by Шаповалов Илья on 19.03.2025.
+//
+
+import Foundation
+import Readers
+import Validated
+
+public extension Validated {
+    typealias Parser = Reader<Success, Result<Success, Failure>>
+    
+    @inlinable
+    init(with state: Success, parsers: [Parser]) {
+        self.init(
+            reduce: state,
+            validators: parsers.map { $0.apply(_:) }
+        )
+    }
+}
